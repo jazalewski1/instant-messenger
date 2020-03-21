427fc7f111c6571946fd56385d6f742efc09a096
jazalewski1 <jazalewski1@gmail.com>
2020-03-21 18:30:49 +0100

    Add simple client

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -9,7 +9,91 @@
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello client!\n";
+	if(argc != 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " ipAddress portNumber" << std::endl;
+		return -1;
+	}
+
+	std::string location {argv[1]};
+	int portNumber {std::stoi(argv[2])};
+
+	std::cout << "ip>" << location << "<\n";
+	std::cout << "port>" << portNumber << "<\n";
+
+	// Create socket
+	int sockfd {socket(AF_INET, SOCK_STREAM, 0)};
+	if(sockfd == -1)
+	{
+		std::cerr << "Can't create a socket!" << std::endl;
+		return -1;
+	}
+
+	// Fill hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(portNumber);
+	inet_pton(AF_INET, location.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	std::cout << "Connecting to server.\n\n";
+	int connectResult {connect(sockfd, (sockaddr*)&hint, sizeof(hint))};
+	if(connectResult == -1)
+	{
+		std::cerr << "Can't connect to server!" << std::endl;
+		close(sockfd);
+		return -2;
+	}
+
+	char serverName [NI_MAXHOST];
+	memset(serverName, 0, NI_MAXHOST);
+	char serverServiceName [NI_MAXSERV];
+	memset(serverServiceName, 0, NI_MAXSERV);
+
+	getnameinfo((sockaddr*)&hint, sizeof(hint), 
+				serverName, NI_MAXHOST, 
+				serverServiceName, NI_MAXSERV, 0);
+
+	inet_ntop(AF_INET, &hint.sin_addr, serverName, NI_MAXHOST);
+	std::cout << "Server IP: " << serverName << ", port: " << ntohs(hint.sin_port) << "\n\n";
+
+	unsigned int bufferSize {4096};
+	char receiveBuffer [bufferSize];
+	std::string input;
+
+	std::cout << "(type \"/close\" to disconnect)\n\n";
+
+	do
+	{
+		std::cout << "YOU> ";
+		getline(std::cin, input);
+
+		long int sentBytes {send(sockfd, input.c_str(), static_cast<int>(input.length()), 0)};
+		if(sentBytes == -1)
+		{
+			std::cerr << "Error in sending data." << std::endl;
+			break;
+		}
+		else
+		{
+			memset(receiveBuffer, 0, bufferSize);
+
+			long int receivedBytes {recv(sockfd, receiveBuffer, static_cast<int>(bufferSize), 0)};
+			std::cout << "bytes received: " << receivedBytes << "\n";
+			if(receivedBytes == -1)
+			{
+				std::cerr << "Error receiving data." << std::endl;
+				break;
+			}
+			else
+			{
+				std::cout << "SERVER> " << std::string(receiveBuffer, 0, bufferSize) << "\n";
+			}
+		}
+
+	} while(input != "/close");
+
+	close(sockfd);
 
 	return 0;
 }

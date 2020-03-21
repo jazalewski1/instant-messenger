c9f4ba59d213de934cfe8186caa9b5b20201facd
jazalewski1 <jazalewski1@gmail.com>
2020-03-21 18:08:09 +0100

    Add client app

---
+++ client/src/Main.cpp
@@ -0,0 +1,15 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <netdb.h>
+#include <string>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello client!\n";
+
+	return 0;
+}

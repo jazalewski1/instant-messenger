7f7366e79415c610d02b534bcf6a89bdd4da4536
jazalewski1 <jazalewski1@gmail.com>
2020-03-21 21:12:49 +0100

    Add multiple clients connection, minor fixes

--- server/src/Main.cpp
+++ server/src/Main.cpp
@@ -7,6 +7,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+void displayInfo(const std::string& name, sockaddr_in* saddrPtr)
+{
+	char ip [NI_MAXHOST];
+	memset(ip, 0, NI_MAXHOST);
+	char port [NI_MAXSERV];
+	memset(port, 0, NI_MAXSERV);
+
+	getnameinfo((sockaddr*)&saddrPtr, sizeof(sockaddr_in), 
+				ip, NI_MAXHOST, 
+				nullptr, 0, 0);
+
+	inet_ntop(AF_INET, &saddrPtr->sin_addr, ip, NI_MAXHOST);
+	std::cout << name << "IP: " << ip << ", port: " << ntohs(saddrPtr->sin_port) << "\n";
+}
+
+
 int main(int argc, char* argv[])
 {
 	if(argc != 2)
@@ -34,19 +50,22 @@ int main(int argc, char* argv[])
 	}
 
 	// Iterate through list to find possible address
-	int sockfd;
+	int sockfdListener;
 	addrinfo* result;
 	for(result = listBegin; result != nullptr; result = result->ai_next)
 	{
-		sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-		if(sockfd == -1)
+		sockfdListener = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		if(sockfdListener == -1)
 			continue;
 
-		int bindResult {bind(sockfd, result->ai_addr, result->ai_addrlen)};
-		if(bindResult == 0)
-			break;
+		int bindResult {bind(sockfdListener, result->ai_addr, result->ai_addrlen)};
+		if(bindResult == -1)
+		{
+			close(sockfdListener);
+			continue;
+		}
 
-		close(sockfd);
+		break;
 	}
 
 	if(result == nullptr)
@@ -56,92 +75,100 @@ int main(int argc, char* argv[])
 
 	freeaddrinfo(listBegin);
 
-	// Print server info
-	char serverName [NI_MAXHOST];
-	memset(&serverName, 0, NI_MAXHOST);
-	char serverServiceName [NI_MAXSERV];
-	memset(&serverServiceName, 0, NI_MAXSERV);
-
-	sockaddr_in* saddrin;
-	saddrin = (sockaddr_in*)result->ai_addr;
-	inet_ntop(AF_INET, (in_addr*)&saddrin->sin_addr, serverName, NI_MAXHOST);
-	std::cout << "Server IP: " << serverName << ", port: " << portNumber << "\n\n";
+	std::cout << "Server ";
+	displayInfo("Server", (sockaddr_in*)result->ai_addr);
 
 	// Listen for connection
-	int listenResult {listen(sockfd, SOMAXCONN)};
+	int listenResult {listen(sockfdListener, 3)};
 	if(listenResult == -1)
 	{
 		std::cerr << "Error listening!" << std::endl;
 		return -4;
 	}
 
-	sockaddr_in hostHint;
-	socklen_t hostSize {sizeof(hostHint)};
+	fd_set master;
+	FD_ZERO(&master);
+	FD_SET(sockfdListener, &master);
 
-	int sockfdHost {accept(sockfd, (sockaddr*)&hostHint, &hostSize)};
-	if(sockfdHost == -1)
-	{
-		std::cerr << "Error accepting client!" << std::endl;
-		return -5;
-	}
+	int sockfdMax {sockfdListener};
 
-	// Close listening socket
-	close(sockfd);
+	unsigned int maxBufferSize {4096};
+	char buffer [maxBufferSize];
 
-	// Print client info
-	char hostName [NI_MAXHOST];
-	memset(hostName, 0, NI_MAXHOST);
-	char hostServiceName [NI_MAXSERV];
-	memset(hostServiceName, 0, NI_MAXSERV);
-
-	getnameinfo((sockaddr*)&hostHint, hostSize, 
-				hostName, NI_MAXHOST, 
-				hostServiceName, NI_MAXSERV, 0);
-
-	inet_ntop(AF_INET, &hostHint.sin_addr, hostName, NI_MAXHOST);
-	std::cout << "Client IP: " << hostName << ", port: " << ntohs(hostHint.sin_port) << std::endl;
-
-	// Start receiving/sending data
-	unsigned int bufferSize {4096};
-	char buffer [bufferSize];
-	
+	std::cout << "Entering main loop.\n\n";
 	while(true)
 	{
-		memset(buffer, 0, bufferSize);
-
-		long int bytesReceived {recv(sockfdHost, buffer, bufferSize, 0)};
-		if(bytesReceived == -1)
+		fd_set copySet {master};
+		int socketCount {select(sockfdMax + 1, &copySet, nullptr, nullptr, nullptr)};
+		if(socketCount == -1)
 		{
-			std::cerr << "Error receiving data!" << std::endl;
-			break;
+			std::cerr << "Error selecting!" << std::endl;
+			break;	
 		}
-		else if(bytesReceived == 0)
-		{
-			std::cout << "Client disconnected. Closing." << std::endl;
-			break;
-		}
-		else
-		{
-			std::string message {buffer, 0, static_cast<long unsigned int>(bytesReceived)};
-			std::cout << "CLIENT> " << message << std::endl;
 
-			std::string confirmMsg {"Message received"};
+		for(int sockX {0}; sockX <= sockfdMax; ++sockX)
+		{
+			if(FD_ISSET(sockX, &copySet))
+			{
+				if(sockX == sockfdListener) // Listening socket 
+				{
+					sockaddr_storage remoteAddr;
+					socklen_t addrSize {sizeof(remoteAddr)};
+					int newfd {accept(sockfdListener, (sockaddr*)&remoteAddr, &addrSize)};
 
-			long int sentBytes {send(sockfdHost, confirmMsg.c_str(), static_cast<int>(confirmMsg.length()), 0)};
-			if(sentBytes == -1)
-			{
-				std::cerr << "Error sending data! Disconnecting..." << std::endl;
-				break;
-			}
-			else
-			{
-				std::cout << "Message sent back.\n";
+					if(newfd <= -1)
+					{
+						std::cerr << "Error accepting!" << std::endl;
+					}
+					else
+					{
+						FD_SET(newfd, &master);
+						std::cout << "Connected new client. ";
+						displayInfo("Client", (sockaddr_in*)&remoteAddr);
+						if(newfd > sockfdMax)
+							sockfdMax = newfd;
+					}
+				}
+				else // Get data from clients
+				{
+					long int bytesReceived {recv(sockX, buffer, maxBufferSize, 0)};
+					if(bytesReceived <= -1)
+					{
+						std::cerr << "Error receiving data! Socket #" << sockX << std::endl;
+						close(sockX);
+						FD_CLR(sockX, &master);
+						return -5;
+					}
+					else if(bytesReceived == 0)
+					{
+						std::cout << "Client disconnected. Socket #" << sockX << "\n";
+						close(sockX);
+						FD_CLR(sockX, &master);
+					}
+					else
+					{
+						// Send data to other clients
+						for(int sockY {0}; sockY <= sockfdMax; ++sockY)
+						{
+							if(FD_ISSET(sockY, &master))
+							{
+								if(sockY != sockfdListener && sockY != sockX) // Don't send to listening socket and itself
+								{
+									long int bytesSent {send(sockY, buffer, bytesReceived, 0)};
+									if(bytesSent <= -1)
+									{
+										std::cerr << "Error sending data! From Socket #" << sockX << " to " << sockY << std::endl;
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
 
-
-	close(sockfdHost);
+	close(sockfdListener);
 
 	return 0;
 }

b0ae6773743461c1ecc3f51e48a3195f2f923c1d
jazalewski1 <jazalewski1@gmail.com>
2020-04-08 20:28:36 +0200

    Move server app's driver to separate class

---
+++ server/app/App.hpp
@@ -0,0 +1,115 @@
+#include <Data.hpp>
+#include <Listener.hpp>
+#include <Server.hpp>
+#include <string>
+#include <thread>
+
+class App
+{
+private:
+	const std::string m_port_number;
+
+	Listener listener;
+	Server server;
+
+	std::thread input_thread;
+
+public:
+	App(std::string port_number) :
+		m_port_number{port_number},
+		listener{},
+		server{&listener}
+	{
+	}
+
+	~App()
+	{
+		if(input_thread.joinable())
+			input_thread.join();
+
+		std::cout << "Closing.\n";
+	}
+
+	int run()
+	{
+		std::cout << "Starting server.\n";
+		if(server.connect(m_port_number) <= -1)
+		{
+			std::cerr << "Error: can't connect the server!\n";
+			return -1;
+		}
+
+		std::cout << "Enter \"/close\" to close server.\n";
+
+		bool is_server_running {true};
+		input_thread  = std::thread{[&]{
+			std::string input;
+			while(is_server_running)
+			{
+				std::getline(std::cin, input);
+				if(input.find("/close") == 0)
+					is_server_running = false;
+			}
+		}};
+
+		while(is_server_running)
+		{
+			int poll_result {server.poll()};
+			if(poll_result == -1)
+			{
+				break;
+			}
+			else if(poll_result == 0)
+			{
+				if(server.addClient() == -1)
+					break;
+			}
+			else if(poll_result > 0)
+			{
+				Data data_received {server.receive(poll_result)};
+
+				if(data_received.bytes == -1)
+				{
+					break;
+				}
+				else if(data_received.bytes == 0)
+				{
+					if(server.removeClient(poll_result) == -1)
+						break;
+				}
+				else
+				{
+					std::cout << "CLIENT #" << poll_result << "> " << data_received.data << std::endl;
+
+					if(data_received.data.find("/sendfile") == 0)
+					{
+						std::string filename {data_received.data.begin() + data_received.data.find(' ') + 1, data_received.data.end()};
+						std::cout << "Request to send file: \"" << filename << "\"" << std::endl;
+
+						server.sendExcept(poll_result, "/receivefile " + filename);
+						std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+						if(server.waitForAcceptFile(poll_result) == -1)
+						{
+							std::cout << "File not accepted." << std::endl;
+							server.sendTo(poll_result, "/rejectfile");
+						}
+						else
+						{
+							std::cout << "File accepted." << std::endl;
+							server.sendTo(poll_result, "/acceptfile");
+
+							std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+							server.transferFile(poll_result);
+						}
+					}
+					else
+					{
+						server.sendExcept(poll_result, data_received.data);
+					}
+				}
+			}
+		}
+	}
+};
--- server/app/Main.cpp
+++ server/app/Main.cpp
@@ -1,8 +1,5 @@
-#include <Data.hpp>
-#include <Listener.hpp>
-#include <Server.hpp>
-#include <string>
-#include <thread>
+#include <App.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
@@ -12,93 +9,9 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	Listener listener {};
+	App app {argv[1]};
 
-	Server server {&listener};
-
-	if(server.connect(argv[1]) <= -1)
-	{
-		std::cerr << "Error: can't connect the server!\n";
-		return -1;
-	}
-
-	std::cout << "Enter \"/close\" to close server.\n";
-
-	bool isServerRunning {true};
-	std::thread inputThread {[&](){
-		std::string input;
-		while(isServerRunning)
-		{
-			std::getline(std::cin, input);
-			if(input.find("/close") == 0)
-				isServerRunning = false;
-		}
-	}};
-
-	while(isServerRunning)
-	{
-		int pollResult {server.poll()};
-		if(pollResult == -1)
-		{
-			break;
-		}
-		else if(pollResult == 0)
-		{
-			if(server.addClient() == -1)
-				break;
-		}
-		else if(pollResult > 0)
-		{
-			Data dataReceived {server.receive(pollResult)};
-
-			if(dataReceived.bytes == -1)
-			{
-				break;
-			}
-			else if(dataReceived.bytes == 0)
-			{
-				if(server.removeClient(pollResult) == -1)
-					break;
-			}
-			else
-			{
-				std::cout << "CLIENT #" << pollResult << "> " << dataReceived.data << std::endl;
-
-				if(dataReceived.data.find("/sendfile") == 0)
-				{
-					std::string filename {dataReceived.data.begin() + dataReceived.data.find(' ') + 1, dataReceived.data.end()};
-					std::cout << "Request to send file: \"" << filename << "\"" << std::endl;
-
-					server.sendExcept(pollResult, "/receivefile " + filename);
-					std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-					if(server.waitForAcceptFile(pollResult) == -1)
-					{
-						std::cout << "File not accepted." << std::endl;
-						server.sendTo(pollResult, "/rejectfile");
-					}
-					else
-					{
-						std::cout << "File accepted." << std::endl;
-						server.sendTo(pollResult, "/acceptfile");
-
-						std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-						server.transferFile(pollResult);
-					}
-				}
-				else
-				{
-					server.sendExcept(pollResult, dataReceived.data);
-				}
-			}
-		}
-	}
-
-	if(inputThread.joinable())
-		inputThread.join();
-
-	std::cout << "Closing.\n";
+	app.run();
 
 	return 0;
 }

ea8489236f38fe4ab67a701eb5e485639b8428c4
jazalewski1 <jazalewski1@gmail.com>
2020-04-09 17:22:28 +0200

    Update server tests

--- tests/source/ServerTest.cpp
+++ tests/source/ServerTest.cpp
@@ -1,3 +1,4 @@
+#include <Exceptions.hpp>
 #include <Server.hpp>
 #include <MockListener.hpp>
 #include <gtest/gtest.h>
@@ -25,10 +26,10 @@ TEST_F(ServerTest, ConnectSuccess)
 	EXPECT_CALL(listener, start_listening)
 	.WillOnce(Return(0));
 
-	EXPECT_EQ(server.connect("9999"), 0);
+	EXPECT_NO_THROW(server.connect("9999"));
 }
 
-TEST_F(ServerTest, ConnectFailure)
+TEST_F(ServerTest, ConnectFailureCreating)
 {
 	EXPECT_CALL(listener, create_listening_socket("9999"))
 	.WillOnce(Return(-1));
@@ -36,10 +37,10 @@ TEST_F(ServerTest, ConnectFailure)
 	EXPECT_CALL(listener, start_listening)
 	.Times(0);
 
-	EXPECT_EQ(server.connect("9999"), -1);
+	EXPECT_THROW(server.connect("9999"), Util::create_socket_error);
 }
 
-TEST_F(ServerTest, ConnectFailure2)
+TEST_F(ServerTest, ConnectFailureListening)
 {
 	EXPECT_CALL(listener, create_listening_socket("9999"))
 	.WillOnce(Return(0));
@@ -47,23 +48,15 @@ TEST_F(ServerTest, ConnectFailure2)
 	EXPECT_CALL(listener, start_listening)
 	.WillOnce(Return(-1));
 
-	EXPECT_EQ(server.connect("9999"), -1);
+	EXPECT_THROW(server.connect("9999"), Util::listen_error);
 }
 
 TEST_F(ServerTest, PollSuccess)
 {
 	EXPECT_CALL(listener, poll)
-	.WillOnce(Return(1));
+	.WillOnce(Return(5));
 
-	EXPECT_EQ(server.poll(), 1);
-}
-
-TEST_F(ServerTest, PollFailure)
-{
-	EXPECT_CALL(listener, poll)
-	.WillOnce(Return(-1));
-
-	EXPECT_EQ(server.poll(), -1);
+	EXPECT_EQ(server.poll(), 5);
 }
 
 TEST_F(ServerTest, ReceiveSuccess)
@@ -73,9 +66,7 @@ TEST_F(ServerTest, ReceiveSuccess)
 	EXPECT_CALL(listener, receive_data)
 	.WillOnce(Return(msg.length()));
 
-	Data expected {static_cast<long int>(msg.length()), msg};
-
-	EXPECT_EQ(server.receive_from(5).bytes, expected.bytes);
+	EXPECT_NO_THROW(server.receive_from(5));
 }
 
 TEST_F(ServerTest, ReceiveFailure)
@@ -83,102 +74,25 @@ TEST_F(ServerTest, ReceiveFailure)
 	EXPECT_CALL(listener, receive_data)
 	.WillOnce(Return(-1));
 
-	Data expected {-1, ""};
-
-	EXPECT_EQ(server.receive_from(5).bytes, expected.bytes);
+	EXPECT_THROW(server.receive_from(5), Util::receive_error);
 }
 
-TEST_F(ServerTest, send_toSuccess)
+TEST_F(ServerTest, SendToSuccess)
 {
 	const std::string msg {"test message"};
 
 	EXPECT_CALL(listener, send_data(5, msg))
 	.WillOnce(Return(msg.length()));
 
-	EXPECT_EQ(server.send_to(5, msg), 0);
+	EXPECT_NO_THROW(server.send_to(5, msg));
 }
 
-TEST_F(ServerTest, send_toFailure)
+TEST_F(ServerTest, SendToFailure)
 {
 	const std::string msg {"test message"};
 
 	EXPECT_CALL(listener, send_data(5, msg))
 	.WillOnce(Return(-1));
 
-	EXPECT_EQ(server.send_to(5, msg), -1);
-}
-
-TEST_F(ServerTest, AcceptFileSuccess1)
-{
-	EXPECT_CALL(listener, poll)
-	.WillOnce(Return(3));
-
-	const std::string acceptMsg {"/accept"};
-
-	EXPECT_CALL(listener, receive_data)
-	.WillOnce(Return(acceptMsg.length()));
-
-	server.wait_for_accept_file(5);
-}
-
-TEST_F(ServerTest, AcceptFileSuccess2)
-{
-	InSequence seq;
-
-	EXPECT_CALL(listener, poll)
-	.Times(3)
-	.WillRepeatedly(Return(-2));
-
-	EXPECT_CALL(listener, poll)
-	.WillOnce(Return(3));
-
-	const std::string acceptMsg {"/accept"};
-
-	EXPECT_CALL(listener, receive_data)
-	.WillOnce(Return(acceptMsg.length()));
-
-	server.wait_for_accept_file(5);
-}
-
-TEST_F(ServerTest, AcceptFileFailure1)
-{
-	EXPECT_CALL(listener, poll)
-	.WillOnce(Return(-1));
-
-	EXPECT_CALL(listener, receive_data)
-	.Times(0);
-
-	server.wait_for_accept_file(5);
-}
-
-TEST_F(ServerTest, AcceptFileFailure2)
-{
-	EXPECT_CALL(listener, poll)
-	.WillOnce(Return(3));
-
-	EXPECT_CALL(listener, receive_data)
-	.WillOnce(Return(-1));
-
-	EXPECT_EQ(server.wait_for_accept_file(5), -1);
-}
-
-TEST_F(ServerTest, transfer_fileFailure1)
-{
-	const std::string msg {"test file"};
-
-	InSequence seq;
-
-	EXPECT_CALL(listener, receive_data)
-	.WillOnce(Return(msg.length()));
-
-	EXPECT_CALL(listener, send_to_except)
-	.Times(1);
-
-	EXPECT_CALL(listener, receive_data)
-	.WillOnce(Return(-1));
-
-	EXPECT_CALL(listener, send_to_except)
-	.Times(0);
-
-	server.transfer_file(5);
+	EXPECT_THROW(server.send_to(5, msg), Util::send_error);
 }

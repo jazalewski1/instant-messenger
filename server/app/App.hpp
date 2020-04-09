74275f1953aef63821d881f2db7d25b163a29802
jazalewski1 <jazalewski1@gmail.com>
2020-04-09 10:52:44 +0200

    Minor variable naming fix

--- server/app/App.hpp
+++ server/app/App.hpp
@@ -9,23 +9,23 @@ class App
 private:
 	const std::string m_port_number;
 
-	Listener listener;
-	Server server;
+	Listener m_listener;
+	Server m_server;
 
-	std::thread input_thread;
+	std::thread m_input_thread;
 
 public:
 	App(std::string port_number) :
 		m_port_number{port_number},
-		listener{},
-		server{&listener}
+		m_listener{},
+		m_server{&m_listener}
 	{
 	}
 
 	~App()
 	{
-		if(input_thread.joinable())
-			input_thread.join();
+		if(m_input_thread.joinable())
+			m_input_thread.join();
 
 		std::cout << "Closing.\n";
 	}
@@ -33,7 +33,7 @@ public:
 	int run()
 	{
 		std::cout << "Starting server.\n";
-		if(server.connect(m_port_number) <= -1)
+		if(m_server.connect(m_port_number) <= -1)
 		{
 			std::cerr << "Error: can't connect the server!\n";
 			return -1;
@@ -42,7 +42,7 @@ public:
 		std::cout << "Enter \"/close\" to close server.\n";
 
 		bool is_server_running {true};
-		input_thread  = std::thread{[&]{
+		m_input_thread  = std::thread{[&]{
 			std::string input;
 			while(is_server_running)
 			{
@@ -54,19 +54,19 @@ public:
 
 		while(is_server_running)
 		{
-			int poll_result {server.poll()};
+			int poll_result {m_server.poll()};
 			if(poll_result == -1)
 			{
 				break;
 			}
 			else if(poll_result == 0)
 			{
-				if(server.add_client() == -1)
+				if(m_server.add_client() == -1)
 					break;
 			}
 			else if(poll_result > 0)
 			{
-				Data data_received {server.receive(poll_result)};
+				Data data_received {m_server.receive(poll_result)};
 
 				if(data_received.bytes == -1)
 				{
@@ -74,7 +74,7 @@ public:
 				}
 				else if(data_received.bytes == 0)
 				{
-					if(server.remove_client(poll_result) == -1)
+					if(m_server.remove_client(poll_result) == -1)
 						break;
 				}
 				else
@@ -86,27 +86,27 @@ public:
 						std::string filename {data_received.data.begin() + data_received.data.find(' ') + 1, data_received.data.end()};
 						std::cout << "Request to send file: \"" << filename << "\"" << std::endl;
 
-						server.send_to_except(poll_result, "/receivefile " + filename);
+						m_server.send_to_except(poll_result, "/receivefile " + filename);
 						std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-						if(server.wait_for_accept_file(poll_result) == -1)
+						if(m_server.wait_for_accept_file(poll_result) == -1)
 						{
 							std::cout << "File not accepted." << std::endl;
-							server.send_to(poll_result, "/rejectfile");
+							m_server.send_to(poll_result, "/rejectfile");
 						}
 						else
 						{
 							std::cout << "File accepted." << std::endl;
-							server.send_to(poll_result, "/acceptfile");
+							m_server.send_to(poll_result, "/acceptfile");
 
 							std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-							server.transfer_file(poll_result);
+							m_server.transfer_file(poll_result);
 						}
 					}
 					else
 					{
-						server.send_to_except(poll_result, data_received.data);
+						m_server.send_to_except(poll_result, data_received.data);
 					}
 				}
 			}
